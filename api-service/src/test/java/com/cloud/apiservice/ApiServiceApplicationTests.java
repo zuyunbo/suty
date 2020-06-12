6217e26ece0d57d97b74acf026ec6af21b290459
zhangty <sy829322@163.com>
2020-06-12 14:57:28 +0800

    提交模块

---
+++ api-service/src/test/java/com/cloud/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ register-center/src/main/java/com/cloud/registercenter/RegisterCenterApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.registercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegisterCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterCenterApplication.class, args);
+    }
+
+}
