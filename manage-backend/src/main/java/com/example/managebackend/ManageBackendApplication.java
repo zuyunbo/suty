b0017d8dd309672599606cfce77171d420c4cbcb
zuyunbo <460895202@qq.com>
2020-06-13 00:53:19 +0800

    矿机

--- manage-backend/src/main/java/com/example/managebackend/ManageBackendApplication.java
+++ manage-backend/src/main/java/com/example/managebackend/ManageBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.managebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.cloud.apiservice.mapper"})
 public class ManageBackendApplication {
 
     public static void main(String[] args) {
