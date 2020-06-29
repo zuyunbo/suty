31e474ed9bf7dee5c39bb54ed7d19801af262201
zuyunbo <460895202@qq.com>
2020-06-29 15:36:16 +0800

    用户端

---
+++ manage-web/src/main/java/com/example/manageweb/ManageWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageWebApplication.class, args);
+    }
+
+}
