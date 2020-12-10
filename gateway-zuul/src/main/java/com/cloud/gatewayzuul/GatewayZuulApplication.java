e5c7a6d27a2dc3e8850c368489070c8bfe6c8400
zuyunbo <460895202@qq.com>
2020-12-10 11:01:43 +0800

    1

---
+++ gateway-zuul/src/main/java/com/cloud/gatewayzuul/GatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+
+}
